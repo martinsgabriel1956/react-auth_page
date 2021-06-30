b05289528b4966aa00393637047a084fba358ade
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-06-30 14:48:48 -0300

    feat: using the context API

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import { GlobalStyles } from './styles/global';
+import { GlobalStyles } from "./styles/global";
 
-import { MainHeader } from './components/MainHeader'
+import { MainHeader } from "./components/MainHeader";
 
-import { Home } from './pages/Home';
-import { Login } from './pages/Login';
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { AuthContext } from "./context/authContext";
 
 export function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     const userLogged = localStorage.getItem("isLoggedIn");
-    if(userLogged === "2") setIsLoggedIn(true);
+    if (userLogged === "2") setIsLoggedIn(true);
   }, []);
-  
+
   function handleLogin(email, password) {
-    localStorage.setItem('isLoggedIn', "2");
+    localStorage.setItem("isLoggedIn", "2");
     setIsLoggedIn(true);
   }
-  
+
   function handleLogout() {
-    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   }
 
   return (
-    <>
+    <AuthContext.Provider value = {{ isLoggedIn }}>
       <GlobalStyles />
       <MainHeader isAuthenticated={isLoggedIn} onLogout={handleLogout} />
       <main>
         {!isLoggedIn && <Login onLogin={handleLogin} />}
         {isLoggedIn && <Home onLogout={handleLogout} />}
       </main>
-    </>
+    </AuthContext.Provider>
   );
 }
